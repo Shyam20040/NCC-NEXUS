e9d0b75928ad790a283b3d8666ffb764bbb2d615
lavanya-jain13 <er.jainlavanya@gmail.com>
2026-01-02 15:58:50 +0530

    Added-Auth-Route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login } from "../controllers/auth.controller.js";
+import { resetPasswordLoggedIn } from "../controllers/auth.controller.js";
 import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
